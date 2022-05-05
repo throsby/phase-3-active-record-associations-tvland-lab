508567f924811c52545df2ad16e47df38843d158
Throsby Wells <throsbywells@gmail.com>
2022-05-05 11:00:56 -0400

    commit tree e5140ed97bb23552cbf4f7a623047d7c56e7e66e

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    belongs_to :network
+    
+
+    def actors_list        
+        self.characters.map do |t|
+            p t.actor.full_name
+        end       
+    end
+    
 end
